08bfce97ea03682a06ba66558e5c6bf4c01e4d06
yaseenalgailani <yaseenalgailani@gmail.com>
2018-09-17 13:17:50 +1000

    improve XBrowser support

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,7 +1,7 @@
 function viewer(index) {
   var vline=document.querySelector(".vLine");
   var content=document.querySelectorAll("section");
-  for(let i=1;i<content.length;i++){
+  for(var i=1;i<content.length;i++){
     content[i].classList.remove("display");
   }
   content[index].classList.add("display");
