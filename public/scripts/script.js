b01b87d218f0276bf4fef1a405b1a6aa31c07c7b
yaseenalgailani <yaseenalgailani@gmail.com>
2018-09-24 09:44:01 +1000

    testing something

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,21 +1,44 @@
-function viewer(index) {
-  var vline=document.querySelector(".vLine");
-  var content=document.querySelectorAll("section");
-  for(var i=1;i<content.length;i++){
-    content[i].classList.remove("display");
-  }
-  content[index].classList.add("display");
-  vline.classList.add("vLdisplay");
-}
+window.onload=function(){
+
+    function viewer(index) {
+      var vline=document.querySelector(".vLine");
+      var content=document.querySelectorAll("section");
+      for(var i=1;i<content.length;i++){
+        content[i].classList.remove("display");
+      }
+      content[index].classList.add("display");
+      vline.classList.add("vLdisplay");
+    }
 
 
-function togg() {
-  var mainBtnOut=document.querySelector(".mainBtnOut");
-  var mainBtnIn=document.querySelector(".mainBtnIn");
-  var navBtn=document.querySelectorAll(".nav-btn");
-  mainBtnOut.classList.toggle("navPulseIn");
-  mainBtnIn.classList.toggle("navPulse");
-  for (var i=0;i<navBtn.length;i++){
-    navBtn[i].classList.toggle("navDisplay");
-  }
+    function togg() {
+      var mainBtnOut=document.querySelector(".mainBtnOut");
+      var mainBtnIn=document.querySelector(".mainBtnIn");
+      var navBtn=document.querySelectorAll(".nav-btn");
+      mainBtnOut.classList.toggle("navPulseIn");
+      mainBtnIn.classList.toggle("navPulse");
+      for (var i=0;i<navBtn.length;i++){
+        navBtn[i].classList.toggle("navDisplay");
+      }
+    }
+
+
+    (function(){
+      var container=document.getElementById("em-container");
+      var httpRequest = new XMLHttpRequest();
+
+      httpRequest.open("GET", "https://yaseenalgailani.github.io/hello.html", true);
+      httpRequest.setRequestHeader('Content-type', 'text/html');
+      httpRequest.send();
+
+      httpRequest.onreadystatechange=function(){
+        if(httpRequest.readyState==4 && httpRequest.status==200){
+          container.innerHTML=httpRequest.responseText;
+        }
+      }
+    })();
+
+
+
+
 }
