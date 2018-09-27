8ea79001ef79d041f233026ee6a86440b79ef76d
yaseenalgailani <yaseenalgailani@gmail.com>
2018-09-27 17:06:24 +1000

    improve scroll animation

--- public/scripts/mytools.js
+++ public/scripts/mytools.js
@@ -2,6 +2,7 @@
 var articleFrontEnd=document.getElementById('front-end');
 var articleBackend=document.getElementById('backend');
 var articleMostImp=document.getElementById('most-imp');
+var scrollNote=document.getElementById('scroll-note');
 
 var locker=[false,false,false,false,false,false];
 var checkpoint1=0.3;
@@ -12,7 +13,9 @@ function taskRunner(checkpoint,x,y,element,className){
   if(document.documentElement.scrollTop>=(Math.ceil(document.documentElement.scrollHeight*checkpoint))){
     if(!locker[x]){
       locker[x]=true;
-      element.classList.add(className);
+      for(var i=0 ; i<element.length;i++){
+        element[i].classList.add(className[i]);
+      }
       // console.log(">"+checkpoint);
     }
     locker[y]=false;
@@ -20,7 +23,9 @@ function taskRunner(checkpoint,x,y,element,className){
   else if(document.documentElement.scrollTop<=(Math.ceil(document.documentElement.scrollHeight*checkpoint))){
     if(!locker[y]){
       locker[y]=true;
-      element.classList.remove(className);
+      for(var j=0 ; j<element.length;j++){
+        element[j].classList.remove(className[j]);
+      }
       // console.log("<"+checkpoint);
     }
     locker[x]=false;
@@ -28,9 +33,9 @@ function taskRunner(checkpoint,x,y,element,className){
 }
 
 window.addEventListener('scroll', function(){
-  taskRunner(0.1,0,1,articleFrontEnd,'article-in');
-  taskRunner(0.4,2,3,articleBackend,'article-in');
-  taskRunner(0.6,4,5,articleMostImp,'article-in');
+  taskRunner(0.1,0,1,[articleFrontEnd,scrollNote],['article-in',"scroll-hide"]);
+  taskRunner(0.4,2,3,[articleBackend],['article-in']);
+  taskRunner(0.6,4,5,[articleMostImp],['article-in']);
   // if(document.documentElement.scrollTop>=(Math.ceil(document.documentElement.offsetHeight*checkpoint1))){
   //   if(!lockTask1){
   //     lockTask1=true;
