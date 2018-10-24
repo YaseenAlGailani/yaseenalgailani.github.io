e921ca39004f20a1ebb46a1b6492d56c72126396
yaseenalgailani <yaseenalgailani@gmail.com>
2018-10-24 12:20:05 +1100

    es6 to es5

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,71 +1,83 @@
-let introP2=document.querySelector('.intro-p2');
-let aboutImage=document.querySelector(".about-image-wrap");
-let aboutText=document.querySelector(".about-text");
-let aboutH2=document.querySelector(".about-h2");
-let aboutCont=document.querySelector(".about-cont");
-let brgr=document.querySelector(".brgr-index");
-let bars=document.querySelectorAll('.bars-wrap div');
-let mainIndex=document.querySelector('.main-index');
-let arrow=document.querySelector('.arrow');
-let sideArrow=document.querySelector('.side-arrow');
-let sideCircles=document.querySelector('.side-circles-wrap');
+"use strict";
+
+var introP2 = document.querySelector('.intro-p2');
+var aboutImage = document.querySelector(".about-image-wrap");
+var aboutText = document.querySelector(".about-text");
+var aboutH2 = document.querySelector(".about-h2");
+var aboutCont = document.querySelector(".about-cont");
+var brgr = document.querySelector(".brgr-index");
+var bars = document.querySelectorAll('.bars-wrap div');
+var mainIndex = document.querySelector('.main-index');
+var arrow = document.querySelector('.arrow');
+var sideArrow = document.querySelector('.side-arrow');
+var sideCircles = document.querySelector('.side-circles-wrap');
 
 // --------- arrow svg ---------------
-window.setTimeout(()=>{arrow.classList.add('arrow-anim');},2000);
-
+window.setTimeout(function () {
+  arrow.classList.add('arrow-anim');
+}, 2000);
 
 // ------------ burger ---------------
-let unfolder=()=>{
+var unfolder = function unfolder() {
   brgr.classList.toggle('unfold');
-}
+};
 
 // ------------ scroll reveal effects  ---------------
-let checkpoint=document.documentElement.clientWidth <= 768? 0.2 : 0.4;
-let lock=[false,false];
-window.addEventListener("scroll", ()=>{
-  if(document.documentElement.scrollTop>=(Math.ceil(document.documentElement.scrollHeight*checkpoint)) || document.body.scrollTop>=(Math.ceil(document.body.scrollHeight*checkpoint))){
-    if(!lock[0]){
-      lock[0]=true;
-
-      bars.forEach((bar)=>{bar.classList.add('bar-anim-left')});
+var checkpoint = document.documentElement.clientWidth <= 768 ? 0.2 : 0.4;
+var lock = [false, false];
+window.addEventListener("scroll", function () {
+  if (document.documentElement.scrollTop >= Math.ceil(document.documentElement.scrollHeight * checkpoint) || document.body.scrollTop >= Math.ceil(document.body.scrollHeight * checkpoint)) {
+    if (!lock[0]) {
+      lock[0] = true;
+      // console.log(checkpoint);
+      bars.forEach(function (bar) {
+        bar.classList.add('bar-anim-left');
+      });
       aboutImage.classList.add('about-image-wrap-anim');
       aboutText.classList.add('about-text-anim');
       aboutH2.classList.add('about-h2-anim');
       sideArrow.classList.add('side-arrow-in');
-      window.setTimeout(()=>{arrow.classList.remove('arrow-anim');},1000);
-      lock[1]=false;
-    }}
-    else if(document.documentElement.scrollTop<(Math.ceil(document.documentElement.scrollHeight*checkpoint)) || document.body.scrollTop<(Math.ceil(document.body.scrollHeight*checkpoint))){
-      if(!lock[1]){
-        lock[1]=true;
-
-        window.setTimeout(()=>{
-          bars.forEach((bar)=>{bar.classList.remove('bar-anim-left')});
-          aboutImage.classList.remove('about-image-wrap-anim');
-          aboutText.classList.remove('about-text-anim');
-          aboutH2.classList.remove('about-h2-anim');}, 0);
-          brgr.classList.remove('unfold');
-          sideArrow.classList.remove('side-arrow-in');
-
-
-
-        lock[0]=false;
-      }
+      window.setTimeout(function () {
+        arrow.classList.remove('arrow-anim');
+      }, 1000);
+      lock[1] = false;
     }
-})
+  } else if (document.documentElement.scrollTop < Math.ceil(document.documentElement.scrollHeight * checkpoint) || document.body.scrollTop < Math.ceil(document.body.scrollHeight * checkpoint)) {
+    if (!lock[1]) {
+      lock[1] = true;
+      console.log(document.documentElement.scrollTop);
+      window.setTimeout(function () {
+        bars.forEach(function (bar) {
+          bar.classList.remove('bar-anim-left');
+        });
+        aboutImage.classList.remove('about-image-wrap-anim');
+        aboutText.classList.remove('about-text-anim');
+        aboutH2.classList.remove('about-h2-anim');
+      }, 0);
+      brgr.classList.remove('unfold');
+      sideArrow.classList.remove('side-arrow-in');
+      // window.setTimeout(()=>{arrow.classList.add('arrow-anim');},1000);
+
+
+      lock[0] = false;
+    }
+  }
+});
 
 // --------------- Handle Navigation ---------------
 
-function navigate(link){
+function navigate(link) {
   mainIndex.classList.add('main-index-out');
   sideArrow.classList.remove('side-arrow-in');
-  window.setTimeout(function(){window.location.href = link+'.html';},1000);
+  window.setTimeout(function () {
+    window.location.href = link + '.html';
+  }, 1000);
 }
 
-
+// console.log(document.documentElement.clientWidth <= 768);
 
 // ----------------- Side bar reveal --------------
-sideArrow.addEventListener('click',()=>{
-  sideCircles.classList.toggle('side-circles-reveal')
+sideArrow.addEventListener('click', function () {
+  sideCircles.classList.toggle('side-circles-reveal');
   sideArrow.classList.toggle('side-arrow-anim');
-})
+});
